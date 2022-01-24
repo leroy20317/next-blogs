2291c07102ac04f0b7b52972c0b6b6d96b7cd44a
leroy <leroy.ljc@gmail.com>
2022-01-24 17:11:41 +0800

    tests: comment

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -3,7 +3,7 @@
  * @date: 2022-01-15 15:11
  * @description：评论
  */
-import GitalkComponent from 'gitalk/dist/gitalk-component';
+// import GitalkComponent from 'gitalk/dist/gitalk-component';
 import Gitalk from 'gitalk';
 import 'gitalk/dist/gitalk.css';
 import { useEffect, useRef } from 'react';
@@ -28,21 +28,21 @@ const Comment = () => {
 
   return (
     <div style={{ paddingBottom: 50 }}>
-      <GitalkComponent
-        option={{
-          clientID: 'ca94e280d87a0145c117',
-          clientSecret: 'a1f79cb33e9e77df658c8840b64376d9aee45ca4',
-          // clientID: '76115496854f5aa5eb99',
-          // clientSecret: '57b777f30aaa08bbfecf69004751887ac94372ef',
-          repo: 'blog-comments', // The repository of store comments,
-          owner: 'leroy20317',
-          admin: ['leroy20317'],
-          id: window.location.pathname, // Ensure uniqueness and length less than 50
-          distractionFreeMode: false, // Facebook-like distraction free mode
-        }}
-      />
+      {/* <GitalkComponent */}
+      {/*  option={{ */}
+      {/*    clientID: 'ca94e280d87a0145c117', */}
+      {/*    clientSecret: 'a1f79cb33e9e77df658c8840b64376d9aee45ca4', */}
+      {/*    // clientID: '76115496854f5aa5eb99', */}
+      {/*    // clientSecret: '57b777f30aaa08bbfecf69004751887ac94372ef', */}
+      {/*    repo: 'blog-comments', // The repository of store comments, */}
+      {/*    owner: 'leroy20317', */}
+      {/*    admin: ['leroy20317'], */}
+      {/*    id: window.location.pathname, // Ensure uniqueness and length less than 50 */}
+      {/*    distractionFreeMode: false, // Facebook-like distraction free mode */}
+      {/*  }} */}
+      {/* /> */}
 
-      <div ref={ref}></div>
+      <div ref={ref} />
       <style>{`.gt-container .gt-meta{z-index: initial}`}</style>
     </div>
   );
