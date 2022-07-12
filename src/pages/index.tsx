f76f9c7a2311b33a97c051d18b41d91cad01b77d
leroy <59689098+leroy20317@users.noreply.github.com>
2022-07-12 14:33:05 +0800

    Update index.tsx
    feat: 个人技术生活分享

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -294,7 +294,7 @@ Home.getInitialProps = async ({ store, req, query }) => {
   }
   await store.dispatch(
     saveTDK({
-      title: "Leroy's Blog",
+      title: "个人技术生活分享",
       keywords: 'Leroy,Blog',
       description:
         'Good morning, and in case I don’t see you, good afternoon, good evening, and good night...',
